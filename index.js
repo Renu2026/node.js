27e23588e95ff5164d31e226d0275b177d3c4626
Renu <72297367+Renu2026@users.noreply.github.com>
2021-01-20 16:31:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,131 +1,56 @@
-'use strict';
-const x = module.exports;
-const ESC = '\u001B[';
-const OSC = '\u001B]';
-const BEL = '\u0007';
-const SEP = ';';
-const isTerminalApp = process.env.TERM_PROGRAM === 'Apple_Terminal';
+const generateMarkDown= require("./utils/generateMarkdown")
+// array of questions for user
+const questions = [
+    {
+        "type": "input",
+        "name": "title",
+        "message":"What is project title?"
 
-x.cursorTo = (x, y) => {
-	if (typeof x !== 'number') {
-		throw new TypeError('The `x` argument is required');
-	}
+    },
+    {
+        "type": "input",
+        "name": "description",
+        "message":"What is project description?"
+    },
+    {
+        "type": "list",
+        "name": "license",
+        "message": "what is your project license?",
+        "choices": [
+            "bsd",
+            "mit",
+            "gpl",
+            "apache"
+        ]
+    },
+    
 
-	if (typeof y !== 'number') {
-		return ESC + (x + 1) + 'G';
-	}
+];
 
-	return ESC + (y + 1) + ';' + (x + 1) + 'H';
-};
+// function to write README file
+function writeToFile(fileName, data) {
+}
 
-x.cursorMove = (x, y) => {
-	if (typeof x !== 'number') {
-		throw new TypeError('The `x` argument is required');
-	}
+// function to initialize program
+function init() {
+    console.log("init")
+    var inquirer = require('inquirer');
+    inquirer
+    .prompt(
+        questions
+    )
+    .then(answers => {
+        console.log(answers)
+       console.log( generateMarkDown(answers))
+    })
+    .catch(error => {
+        if(error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        } else {
+        // Something else when wrong
+        }
+    });
+}
 
-	let ret = '';
-
-	if (x < 0) {
-		ret += ESC + (-x) + 'D';
-	} else if (x > 0) {
-		ret += ESC + x + 'C';
-	}
-
-	if (y < 0) {
-		ret += ESC + (-y) + 'A';
-	} else if (y > 0) {
-		ret += ESC + y + 'B';
-	}
-
-	return ret;
-};
-
-x.cursorUp = count => ESC + (typeof count === 'number' ? count : 1) + 'A';
-x.cursorDown = count => ESC + (typeof count === 'number' ? count : 1) + 'B';
-x.cursorForward = count => ESC + (typeof count === 'number' ? count : 1) + 'C';
-x.cursorBackward = count => ESC + (typeof count === 'number' ? count : 1) + 'D';
-
-x.cursorLeft = ESC + 'G';
-x.cursorSavePosition = ESC + (isTerminalApp ? '7' : 's');
-x.cursorRestorePosition = ESC + (isTerminalApp ? '8' : 'u');
-x.cursorGetPosition = ESC + '6n';
-x.cursorNextLine = ESC + 'E';
-x.cursorPrevLine = ESC + 'F';
-x.cursorHide = ESC + '?25l';
-x.cursorShow = ESC + '?25h';
-
-x.eraseLines = count => {
-	let clear = '';
-
-	for (let i = 0; i < count; i++) {
-		clear += x.eraseLine + (i < count - 1 ? x.cursorUp() : '');
-	}
-
-	if (count) {
-		clear += x.cursorLeft;
-	}
-
-	return clear;
-};
-
-x.eraseEndLine = ESC + 'K';
-x.eraseStartLine = ESC + '1K';
-x.eraseLine = ESC + '2K';
-x.eraseDown = ESC + 'J';
-x.eraseUp = ESC + '1J';
-x.eraseScreen = ESC + '2J';
-x.scrollUp = ESC + 'S';
-x.scrollDown = ESC + 'T';
-
-x.clearScreen = '\u001Bc';
-
-x.clearTerminal = process.platform === 'win32' ?
-	`${x.eraseScreen}${ESC}0f` :
-	// 1. Erases the screen (Only done in case `2` is not supported)
-	// 2. Erases the whole screen including scrollback buffer
-	// 3. Moves cursor to the top-left position
-	// More info: https://www.real-world-systems.com/docs/ANSIcode.html
-	`${x.eraseScreen}${ESC}3J${ESC}H`;
-
-x.beep = BEL;
-
-x.link = (text, url) => {
-	return [
-		OSC,
-		'8',
-		SEP,
-		SEP,
-		url,
-		BEL,
-		text,
-		OSC,
-		'8',
-		SEP,
-		SEP,
-		BEL
-	].join('');
-};
-
-x.image = (buf, opts) => {
-	opts = opts || {};
-
-	let ret = OSC + '1337;File=inline=1';
-
-	if (opts.width) {
-		ret += `;width=${opts.width}`;
-	}
-
-	if (opts.height) {
-		ret += `;height=${opts.height}`;
-	}
-
-	if (opts.preserveAspectRatio === false) {
-		ret += ';preserveAspectRatio=0';
-	}
-
-	return ret + ':' + buf.toString('base64') + BEL;
-};
-
-x.iTerm = {};
-
-x.iTerm.setCwd = cwd => OSC + '50;CurrentDir=' + (cwd || process.cwd()) + BEL;
+// function call to initialize program
+init();
